c1f4414bef3e12a21ed04b3ed36b79f09899f944
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-18 18:22:25 +0200

    first assignment

---
+++ week-0/main.js
@@ -0,0 +1,6 @@
+document.write("<h1 id='h1'>Elzero</h1>");
+document.getElementById('h1').style.color='blue';
+document.getElementById('h1').style.fontWeight='bold';
+document.getElementById('h1').style.textAlign='center';
+document.getElementById('h1').style.fontSize='80px';
+document.getElementById('h1').style.fontFamily='arial';
