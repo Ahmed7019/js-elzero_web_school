7338e1bab003af2bb7be289c9b7ab1242655d857
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-20 15:09:12 +0200

    added variabels

---
+++ variable_and_concatenation/main.js
@@ -0,0 +1,13 @@
+let 
+    cardTitle = "Elzero" , 
+    cardDescription = "Elzero Web School",
+    cardDate = "25/10";
+
+let card = `
+    <div>
+    <h3>Hello ${cardTitle}</h3>
+    <p>${cardDescription}</p>
+    <span>${cardDate}</span>
+    </div>
+`;
+
