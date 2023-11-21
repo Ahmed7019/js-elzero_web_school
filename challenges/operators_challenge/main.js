d0f1777da7488782b76c6f38f44537fca790777e
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-21 11:27:45 +0200

    operators challenge

--- challenges/operators_challenge/main.js
+++ challenges/operators_challenge/main.js
@@ -5,8 +5,8 @@ let b = "20";
 let c = 80;
 
 console.log(++a + +b++ + +c++ - +a++);
-// console.log(++a + -b + +c++ - -a++ + +a);
-// console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
+console.log(++a + -b + +c++ - -a++ + +a);
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
 
 /**  first one
  ** [++a] [+]
@@ -19,24 +19,42 @@ console.log(++a + +b++ + +c++ - +a++);
  ** - Value : 20
  ** - Explain : Unary plus & post increment
  ** [+]
- ** - Explain : Arethmatic operation 
+ ** - Explain : Arethmatic operation
  ** [+c++]
  ** - Value : 80
  ** - Explain : Unary plus & post increment
  ** [-]
- ** - Explain : Arethmatic operation [minus] 
+ ** - Explain : Arethmatic operation [minus]
  ** [+a++]
  ** - Value : 11
  ** - Explain : Unary plus & post increment
  */
+
 /**  second one
  ** [++a] [+]
-    - Value:
-    - Explain:
-    [+]
-    - Explain:
- * 
-*/
+ ** - Value: 12
+ ** - Explain: Pre increment
+ ** [+]
+ ** - Explain: Arethmatic operation [plus]
+ ** [-b]
+ ** - Value : -21
+ ** - Explain : Unary negation
+ ** [+]
+ ** - Explain : Arethmatic operation [plus]
+ ** [+c++] c
+ ** - Value : 81
+ ** - Explain : Unary plus & post increment
+ ** [-]
+ ** - Explain : Arethamtic operation [minus]
+ ** [-a++]
+ ** - Value : -12
+ ** - Explain : Unary minus & post increment
+ ** [+]
+ ** - Explain : Arethmatic operation [plus]
+ ** [+a]
+ ** - Value : -10
+ ** - Explain : Unary plus
+ */
 /**  third one
  ** [++a] [+]
     - Value:
