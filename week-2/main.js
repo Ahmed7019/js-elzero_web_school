ae40f3a3d00e5b12b701826130ce7081107f842f
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-21 12:08:32 +0200

    last task

--- week-2/main.js
+++ week-2/main.js
@@ -41,3 +41,18 @@ console.log(++num_ + +"9"); // 20
 
 // Solution Four
 console.log(--num_ * (+true * +"2")); // 20
+
+// Task three
+
+let points = 10;
+
+// Write Your Code Here
+points += +"3";
+
+console.log(points); // 13
+
+// Write Your Code Here
+
+points -= +"5";
+
+console.log(points); // 8;
