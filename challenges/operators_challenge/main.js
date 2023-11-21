49de591eb5514f611aed493ca46a2c2ba5760b59
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-21 10:26:26 +0200

    added challenge one

---
+++ challenges/operators_challenge/main.js
@@ -0,0 +1,36 @@
+
+/*  Challenge 1 */
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - +a++);
+console.log(++a + -b + +c++ - -a++ + +a);
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
+
+/**  first one
+ ** [++a] [+]
+    [++a]
+    - Value:
+    - Explain:
+    [+]
+    - Explain:
+ * 
+*/
+/**  second one
+ ** [++a] [+]
+    - Value:
+    - Explain:
+    [+]
+    - Explain:
+ * 
+*/
+/**  third one
+ ** [++a] [+]
+    - Value:
+    - Explain:
+    [+]
+    - Explain:
+ * 
+*/
