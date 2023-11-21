212a745145391d75c300dbea0ef82c3c1f1ebdb1
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-21 10:35:58 +0200

    solved first challenge

--- challenges/operators_challenge/main.js
+++ challenges/operators_challenge/main.js
@@ -1,4 +1,3 @@
-
 /*  Challenge 1 */
 
 let a = 10;
@@ -6,18 +5,30 @@ let b = "20";
 let c = 80;
 
 console.log(++a + +b++ + +c++ - +a++);
-console.log(++a + -b + +c++ - -a++ + +a);
-console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
+// console.log(++a + -b + +c++ - -a++ + +a);
+// console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
 
 /**  first one
  ** [++a] [+]
-    [++a]
-    - Value:
-    - Explain:
-    [+]
-    - Explain:
- * 
-*/
+ ** [++a]
+ ** - Value: 11
+ ** - Explain: Pre increment
+ ** [+]
+ ** - Explain: Arethmatic opration [plus]
+ ** [+b++]
+ ** - Value : 20
+ ** - Explain : Unary plus & post increment
+ ** [+]
+ ** - Explain : Arethmatic operation 
+ ** [+c++]
+ ** - Value : 80
+ ** - Explain : Unary plus & post increment
+ ** [-]
+ ** - Explain : Arethmatic operation [minus] 
+ ** [+a++]
+ ** - Value : 11
+ ** - Explain : Unary plus & post increment
+ */
 /**  second one
  ** [++a] [+]
     - Value:
