5dd74b6aa0f5543a3a14a826b39a746e165fa435
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-23 09:38:06 +0200

    Task one

---
+++ week-3/main.js
@@ -0,0 +1,19 @@
+// Task one
+// Print the number 100000 using 13 different methods
+
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1_000_00); // 100000
+console.log(100000); // 100000
+console.log(Math.pow(10,5)); // 100000
+console.log(10e4); // 100000
+console.log(10 ** 5); // 100000
+console.log(200 * 500); // 100000
+console.log(5e4 * 2); // 100000
+console.log(2e6 / 20); // 100000
+console.log(Math.ceil(99_999.2)); // 100000
+console.log(Math.round(99_999.5)); // 100000
