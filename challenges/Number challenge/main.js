47aa2d4238c0528436b182ea80f0f8bd0c23a74a
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-23 08:20:19 +0200

    setup js challenge

---
+++ challenges/Number challenge/main.js
@@ -0,0 +1,22 @@
+// Number challenge
+
+let a = 1_00;
+let b = 2_00.5;
+let c = 1e2;
+let d = 2.4;
+
+// Find the smallest Number in All variables And Return Integer
+console.log();
+
+// Use variables a + d One time to get the needed output
+console.log(); // 10000
+
+// Get the integer "2" From d variable with 4 methods
+console.log();
+console.log();
+console.log();
+console.log();
+
+// Use variables b + d To get this values
+console.log(); // 66.67 => String
+console.log(); // 67 => Number
