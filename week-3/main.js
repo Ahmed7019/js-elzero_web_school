4c87cac766a48da9c9646c91dd4499bd54b03a36
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-11-24 22:05:54 +0200

    Skip task three and i'll be back

--- week-3/main.js
+++ week-3/main.js
@@ -9,7 +9,7 @@ console.log(5e4 + 5e4); // 100000
 // Your Solutions
 console.log(1_000_00); // 100000
 console.log(100000); // 100000
-console.log(Math.pow(10,5)); // 100000
+console.log(Math.pow(10, 5)); // 100000
 console.log(10e4); // 100000
 console.log(10 ** 5); // 100000
 console.log(200 * 500); // 100000
@@ -20,6 +20,13 @@ console.log(Math.round(99_999.5)); // 100000
 
 // Task two
 
-// print the number 9007199254740991 usin MIN_SAFE_INTEGER
+// print the number 9007199254740991 using MIN_SAFE_INTEGER
 
-console.log(-1 *(Number.MIN_SAFE_INTEGER)); // 9007199254740991
+console.log(-1 * Number.MIN_SAFE_INTEGER); // 9007199254740991
+
+// Task three
+
+// print the number 16 using MAX_SAFE_INTEGER without using numbers
+console.log(Number.MAX_SAFE_INTEGER );
+
+
