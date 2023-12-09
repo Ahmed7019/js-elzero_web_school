28d575e9ea77926df3e22d54abfb987f8ebf432f
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-09 10:37:36 +0200

    task two

--- week-4/main.js
+++ week-4/main.js
@@ -6,3 +6,13 @@ console.log(userName.slice(0,1).toLowerCase()); // e
 console.log(userName.substring(0,1).toLowerCase()); // e
 console.log(userName.substr(0,1).toLowerCase()); // e
 console.log(userName[0].toLowerCase().repeat(3)); // eee
+
+// Task two
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
