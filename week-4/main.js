19c60e4fe7733ef3c8b02ad7d51e201636fefb1f
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-09 10:01:33 +0200

    Task one

---
+++ week-4/main.js
@@ -0,0 +1,8 @@
+// Task 1 ( // is the outputs required)
+let userName = "Elzero";
+console.log(); // e
+console.log(); // e
+console.log(); // e
+console.log(); // e
+console.log(); // e
+console.log(); // eee
