aa2ce1277fc7d14a7ed841166922487a118d8c53
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-12 10:06:09 +0200

    wrote the challenge

---
+++ challenges/arrays_challenge/main.js
@@ -0,0 +1,20 @@
+/**
+ * Arrays Challenges
+ */
+
+let zero = 0;
+
+let counter = 3;
+
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ammer"];
+
+// Write Code Here
+
+console.log(my); //["Osama", "Elham", "Mazero", "Ahmed"]
+
+// console.log(my.slice("????")); // ["Elham","Mazreo"]
+
+// console.log(); // "Elzero"
+
+// console.log(); // "rO"
+
