c0253038775adb5d1fa112c8fc3a07a418126de5
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-12 10:31:46 +0200

    Task three , used some methods and worked fine

--- challenges/arrays_challenge/main.js
+++ challenges/arrays_challenge/main.js
@@ -10,13 +10,18 @@ let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ammer"];
 
 // Write Code Here
 
-my = my.reverse()
+my = my.reverse();
+let x = [];
+// console.log(my.slice(counter - my.indexOf("Gamal"))); //["Osama", "Elham", "Mazero", "Ahmed"]
 
-console.log(my.slice(counter - my.indexOf("Gamal"))); //["Osama", "Elham", "Mazero", "Ahmed"]
+// console.log(my.slice(counter, my.length - my.indexOf("Gamal"))); // ["Elham","Mazreo"]
+console.log(my);
 
-console.log(my.slice(counter,my.length - my.indexOf("Gamal"))); // ["Elham","Mazreo"]
-
-// console.log(); // "Elzero"
+console.log(
+  x.concat(
+    my[counter].slice(zero, counter - my.indexOf("Gamal")),
+    my[counter + my.indexOf("Gamal")].slice(counter - my.indexOf("Gamal"))
+  ).join("")
+); // "Elzero"
 
 // console.log(); // "rO"
-
