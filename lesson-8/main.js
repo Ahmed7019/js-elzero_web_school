d0052ab2fcefbbd2699c7edd34579f05ff645177
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-13 08:40:16 +0200

    Task three

--- lesson-8/main.js
+++ lesson-8/main.js
@@ -24,3 +24,15 @@ let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
 friends.pop();
 friends.shift();
 console.log(friends); // ["Eman", "Osama"]
+
+// Task three
+
+let arrOne = ["C", "D", "X"];
+let arrTwo = ["A", "B", "Z"];
+let finalArr = [];
+
+// Write One Single Line Of Code
+
+finalArr = finalArr.concat(arrOne, arrTwo).sort().reverse();
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
