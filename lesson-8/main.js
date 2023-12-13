d44b10b8d7f47d19d34d28252bd465a802307cdd
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-13 08:23:14 +0200

    method

--- lesson-8/main.js
+++ lesson-8/main.js
@@ -4,7 +4,7 @@ let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
 // Method 1
-console.log(myFriends.pop()); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.slice(myFriends.indexOf("Ahmed"),myFriends.indexOf("Gamal"))); // ["Ahmed", "Elham", "Osama"];
 
 // Method 2
 console.log("Your Code Here"); // ["Ahmed", "Elham", "Osama"];
