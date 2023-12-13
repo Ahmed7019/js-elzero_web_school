aef6f07f754d5f311dce9ea5380f3ff5083da0ec
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-13 08:21:34 +0200

    arrays tasks

---
+++ lesson-8/main.js
@@ -0,0 +1,10 @@
+// Task one
+
+let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num = 3;
+
+// Method 1
+console.log(myFriends.pop()); // ["Ahmed", "Elham", "Osama"];
+
+// Method 2
+console.log("Your Code Here"); // ["Ahmed", "Elham", "Osama"];
