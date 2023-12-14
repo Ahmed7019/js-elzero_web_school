c456a3da2ffc425b19024b8db7ad5c2ad996f9e2
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-14 11:05:14 +0200

    teams number array

--- challenges/loops_challenge/main.js
+++ challenges/loops_challenge/main.js
@@ -16,10 +16,18 @@ let myEmployees = [
 let newAdmins = [];
 
 for (let i = 0; i < myAdmins.length; i++) {
+  // Add the admins to new array
   if (myAdmins[i] === "Stop") {
     break;
   }
   newAdmins[i] = myAdmins[i];
 }
-console.log(newAdmins);
+
+let teamsNumber = [];
+// The number of the teams
+for (let i = 0; i < newAdmins.length; i++) {
+  teamsNumber.push(i);
+}
+
+console.log(teamsNumber);
 document.write(`<div>We Have ${newAdmins.length} Admins</div>`);
