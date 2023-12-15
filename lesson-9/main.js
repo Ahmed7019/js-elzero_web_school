dbd65100f6cdfc391c8ce70687650ae2d4ff090d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-15 08:49:33 +0200

    Task two

--- lesson-9/main.js
+++ lesson-9/main.js
@@ -1,13 +1,13 @@
 // Task one
 
-let start = 10;
-let end = 100;
-let exclude = 40;
+// let start = 10;
+// let end = 100;
+// let exclude = 40;
 
-for (start; start <= end; start += 10) {
-  if (start === 40) continue;
-  console.log(start);
-}
+// for (start; start <= end; start += 10) {
+//   if (start === 40) continue;
+//   console.log(start);
+// }
 // Output
 // 10
 // 20
@@ -18,3 +18,26 @@ for (start; start <= end; start += 10) {
 // 80
 // 90
 // 100
+
+// Task two
+
+let start = 10;
+let end = 0;
+let stop = 3;
+
+for (start; start >= stop; start--) {
+  if (start < 10) {
+    console.log(`${end}${start}`);
+  } else {
+    console.log(start);
+  }
+}
+// Output
+// 10
+// 09
+// 08
+// 07
+// 06
+// 05
+// 04
+// 03
