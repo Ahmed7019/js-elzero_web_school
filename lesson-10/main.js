8a8bb72efdbb0e44833aac555ff0a0898fd07ff2
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-16 08:50:59 +0200

    added for loop and if conditions

---
+++ lesson-10/main.js
@@ -0,0 +1,19 @@
+// While loop assignment
+
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+
+while (counter < friends.length) {
+  if (
+    friends[counter][index] === "A" ||
+    typeof friends[counter] === typeof counter
+  )
+    continue;
+  else {
+    console.log(`${counter + friends.indexOf("Sayed")} => ${friends[counter]}`);
+  }
+}
+// Output
+("1 => Sayed");
+("2 => Mahmoud");
