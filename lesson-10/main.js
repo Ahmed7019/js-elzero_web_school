8bdb421d3120d0b79a8f38994fa5c03cb984aeee
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-16 21:44:22 +0200

    remove the useless code & adding comments

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -4,18 +4,8 @@ let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
 let index = 0;
 let counter = 0;
 let number = [];
-// while (counter < friends.length) {
-//   if (friends[counter][index] === "A") {
-//     counter++;
-//     continue;
-//   } else if (typeof friends[counter] === typeof index) {
 
-//     counter++;
-//   } else {
-//     console.log(`${number[index]} => ${friends[counter]}`);
-//     counter++;
-//   }
-
+// Here add the numbers to the array called number
 for (let i = index; i < friends.length; i++) {
   if (typeof friends[i] === typeof index) {
     number.push(friends[i]);
@@ -23,11 +13,6 @@ for (let i = index; i < friends.length; i++) {
 }
 
 while (counter < friends.length) {
-  /*if (typeof friends[counter] === typeof index) {
-    // Adds the numbers (1 ,2) to number array
-    number.push(friends[counter]);
-    counter++;
-  }*/
   if (friends[counter][index] === "S" || friends[counter][index] === "M") {
     let i = index;
     if (friends[counter][index] === "M") i = friends.indexOf("Sayed");
