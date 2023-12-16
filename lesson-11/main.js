6dc29de35563c03bb7310cf4ef3f7b40933054be
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-16 22:02:19 +0200

    Task two done

--- lesson-11/main.js
+++ lesson-11/main.js
@@ -11,3 +11,24 @@ function sayHello(theName, theGender) {
 sayHello("Osama", "Male"); // "Hello Mr Osama"
 sayHello("Eman", "Female"); // "Hello Miss Eman"
 sayHello("Sameh"); // "Hello Sameh"
+
+// Task two
+// Write a function that make arithmatic operations
+function calculate(firstNum, secondNum, operation) {
+  // Your Code Here
+  if (firstNum === undefined || secondNum === undefined) {
+    console.log("Second Number Not Found");
+    return;
+  }
+  if (operation === "add") console.log(firstNum + secondNum);
+  else if (operation === "subtract") console.log(firstNum - secondNum);
+  else if (operation === "multiply") console.log(firstNum * secondNum);
+  else console.log(firstNum + secondNum);
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, "add"); // 50
+calculate(20, 30, "subtract"); // -10
+calculate(20, 30, "multiply"); // 600
