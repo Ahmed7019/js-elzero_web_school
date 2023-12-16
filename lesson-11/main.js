a8ed9c48607553f05ae601de6bfd439597a6162d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-16 23:07:07 +0200

    task six done :)

--- lesson-11/main.js
+++ lesson-11/main.js
@@ -122,12 +122,13 @@ function multiply(...num) {
   let result = 1;
   for (let i = 0; i < num.length; i++) {
     if (typeof num[i] === typeof "A") continue;
-    else if (Number.isInteger(num[i]) !== true) Math.floor(num[i]);
+    else if (Number.isInteger(num[i]) === false) {
+     num[i] = Math.floor(num[i]);
+    }
     result *= num[i];
   }
   console.log(result);
 }
-
 multiply(10, 20); // 200
 multiply("A", 10, 30); // 300
 multiply(100.5, 10, "B"); // 1000
