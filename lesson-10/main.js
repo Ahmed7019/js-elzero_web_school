c336fa01e05b4d9363c241558e1d455358f4722f
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-16 09:55:16 +0200

    put the number before the name

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -3,18 +3,38 @@
 let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
 let index = 0;
 let counter = 0;
+let number = [];
+// while (counter < friends.length) {
+//   if (friends[counter][index] === "A") {
+//     counter++;
+//     continue;
+//   } else if (typeof friends[counter] === typeof index) {
 
+//     counter++;
+//   } else {
+//     console.log(`${number[index]} => ${friends[counter]}`);
+//     counter++;
+//   }
 while (counter < friends.length) {
-  if (
-    friends[counter][index] === "A" ||
-    typeof friends[counter] === typeof counter
-  )
+  if (typeof friends[counter] === typeof index) {
+    for (let i = 0; i < friends.indexOf("Sayed"); i++)
+      number.push(friends[counter]);
+    counter++;
+  } else if (
+    friends[counter][index] === "S" ||
+    friends[counter][index] === "M"
+  ) {
+    for (let i = 0; i < friends.indexOf("Sayed"); i++)
+      console.log(
+        `${number[i] + friends.indexOf("Sayed")} => ${friends[counter]}`
+      );
+    counter++;
+  } else {
+    counter++;
     continue;
-  else {
-    console.log(`${counter + friends.indexOf("Sayed")} => ${friends[counter]}`);
   }
-  counter++;
 }
+console.log(number);
 // Output
-("1 => Sayed");
-("2 => Mahmoud");
+// ("1 => Sayed");
+// ("2 => Mahmoud");
