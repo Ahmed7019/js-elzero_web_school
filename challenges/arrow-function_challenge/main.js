4f5c89da2b1cbc09db29f6cf5a3c428f7bd53101
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-17 11:55:15 +0200

    added the output needed

---
+++ challenges/arrow-function_challenge/main.js
@@ -0,0 +1,16 @@
+// Challenge one
+
+/*
+  Function Arrow Challenge
+*/
+
+// [1] One Statement In Function
+// [2] Convert To Arrow Function
+// [3] Print The Output [Arguments May Change]
+
+let names = function (...name) {
+  // Parameter ?
+  return `String ${name} => Done !`;
+};
+
+console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
