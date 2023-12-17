9f48abb5c0a2235ade0e867653aa4f36ea7d3f1a
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-17 12:08:22 +0200

    created one function

--- challenges/arrow-function_challenge/main.js
+++ challenges/arrow-function_challenge/main.js
@@ -8,9 +8,15 @@
 // [2] Convert To Arrow Function
 // [3] Print The Output [Arguments May Change]
 
-let names = function (...name) {
+let name = function (...users) {
+  for (let i = 0; i < users.length; i++) {
+    users[i] = `[${users[i]}]`;
+  }
+  return users;
+};
+let names = function (name) {
   // Parameter ?
-  return `String ${name} => Done !`;
+  //   return `String ${name.join("], ")} => Done !`;
 };
 
 console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
