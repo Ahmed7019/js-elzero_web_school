503f5956af514cead9e27be65a28d0101ee0b249
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2023-12-19 21:20:36 +0200

    start over

--- lesson-12/main.js
+++ lesson-12/main.js
@@ -1,32 +1,25 @@
 // Task one
 
+let a = "Osama Mohammed";
+a.split();
+console.log(`${a[0]} ${a[1][0]}.`);
 function getDetails(zName, zAge, zCountry) {
   function namePattern(zName) {
     // Write Your Code Here
-    zName = zName.split();
-    return `${zName[0]} ${zName[1][0]}.`;
     // Osama Mohamed => Osama M.
     // Ahmed ali => Ahmed A.
   }
   function ageWithMessage(zAge) {
     // Write Your Code Here
-    let num = 0;
-    for (let i = 0; i < zAge.length; i++) {
-      if (zAge[i] === typeof num) num = zAge[i];
-    }
-    return num;
     // 38 Is My Age => Your Age Is 38
     // 32 Is The Age => Your Age Is 32
   }
   function countryTwoLetters(zCountry) {
     // Write Your Code Here
-    zCountry = zCountry.slice(0, 3).toUpperCase();
-    return zCountry;
     // Egypt => You Live In EG
     // Syria => You Live In SY
   }
   function fullDetails() {
-    return `Hello ${namePattern()}, Your Age Is ${ageWithMessage()}, You Live In ${countryTwoLetters()}`;
     // Write Your Code Here
   }
   return fullDetails(); // Do Not Edit This
