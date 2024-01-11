d4cfe328eab9255e3ddef54ac22b93b666021155
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-11 08:28:50 +0200

    filtered function

--- challenges/higher-order_functions_challenge/main.js
+++ challenges/higher-order_functions_challenge/main.js
@@ -20,8 +20,6 @@
 let myString = "1,2,3,EE,K,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
 
 let newString = myString.split("").filter(function (ele) {
-  console.log(ele);
-  return isNaN(ele);
+  return ele !== "," && isNaN(ele) ? ele : "";
 }, 10);
-
 console.log(newString);
