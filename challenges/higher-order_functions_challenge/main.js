ba56e5ba4698c3720319b7907b27e866807e3b1c
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-11 08:04:35 +0200

    filter function

--- challenges/higher-order_functions_challenge/main.js
+++ challenges/higher-order_functions_challenge/main.js
@@ -11,10 +11,17 @@
  * - Numbers
  * - Letters
  *
- * -You Must Use [Filter + Mao + Reduce + Your Knowledge]
+ * - You Must Use [Filter + Map + Reduce + Your Knowledge]
  * - Order Is Not Important
  * - All In One Chain
  *
  */
 
 let myString = "1,2,3,EE,K,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let newString = myString.split("").filter(function (ele) {
+  console.log(ele);
+  return isNaN(ele);
+}, 10);
+
+console.log(newString);
