3446ff97a5256a233119d7434ea3cd900b0731f1
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-13 10:06:02 +0200

    task five solved

--- main.js
+++ main.js
@@ -74,4 +74,13 @@ console.log(numbersOnly);
 
 let nums = [2, 12, 11, 5, 10, 1, 99];
 
+let sumNums = nums.reduce((acc, current) => {
+    if(current % 2 === 0) {
+        return acc * current;
+    }else 
+    return acc + current;
+}, 1);
+
 // 500
+
+console.log(sumNums)
