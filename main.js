3ec8cd9bf445878ebfe146b3c460301deda877f4
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-13 06:53:12 +0200

    return letters only

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
 
 let mixLetters = mix.map(function(ele){
-    return !isNaN(ele) ? ele : "";
+    return isNaN(ele) ? ele : "";
 },10).reduce(function(acc,current){
     return `${acc}${current}`
 });
