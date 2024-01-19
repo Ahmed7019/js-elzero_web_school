aed29e2b4293c3ec448a0a792ef704524f6874fa
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-19 16:46:00 +0200

    create header

---
+++ challenges/DOM_challenge/main.js
@@ -0,0 +1,14 @@
+// DOM challenge
+
+// Create page with 15 cards , header ,and footer
+
+// Create header
+
+let header = document.createElement("header"),
+  menu = document.createElement("nav"),
+  logoName = document.createElement("h2"),
+  meunLinks = document.createElement("a");
+
+header.appendChild(menu);
+menu.appendChild(logoName);
+menu.appendChild(meunLinks);
