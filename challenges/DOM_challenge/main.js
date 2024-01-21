b2081e397bfc5d9a7a5d2f821474f61873dc95c6
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-21 09:27:52 +0200

    created text and elements + arrays

--- challenges/DOM_challenge/main.js
+++ challenges/DOM_challenge/main.js
@@ -7,10 +7,25 @@
 let header = document.createElement("header"),
   menu = document.createElement("nav"),
   logoName = document.createElement("h2"),
-  meunLinks = document.createElement("a");
+  // Links here
+  homeLink = document.createElement("a"),
+  aboutLink = document.createElement("a"),
+  serviceLink = document.createElement("a"),
+  contactLink = document.createElement("a");
+
+// Create array of links
+links = [homeLink, aboutLink, serviceLink, contactLink];
+// Header text
+let logoText = document.createTextNode("Elzero"),
+  homeText = document.createTextNode("Home"),
+  aboutText = document.createTextNode("About"),
+  serviceText = document.createTextNode("Service"),
+  contactText = document.createTextNode("Contact");
+// Create menu links text
+linksText = [homeText, aboutText, serviceText, contactText];
 
 header.appendChild(menu);
 menu.appendChild(logoName);
 menu.appendChild(meunLinks);
-
+logoName.appendChild(logoText);
 document.body.appendChild(header);
