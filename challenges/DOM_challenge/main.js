4ec6c3b675cd2cb38ef81a08c4dfc8e95b07b3e0
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-22 09:54:34 +0200

    added more styles

--- challenges/DOM_challenge/main.js
+++ challenges/DOM_challenge/main.js
@@ -32,6 +32,8 @@ for (let i = 0; i < links.length; i++) {
   links[i].setAttribute("href", "#");
   menu.appendChild(links[i]).appendChild(linksText[i]);
 }
+header.style.setProperty("background-color", "white");
+header.style.setProperty("width", "100%");
 document.body.appendChild(header);
 // Add style to the header
 document.body.style.setProperty("font-family", "arial");
@@ -47,18 +49,42 @@ for (let i = 0; i < links.length; i++) {
 
 // Create content
 
-let content = document.createElement("div");
+let content = document.createElement("div"),
+  main = document.createElement("main");
 for (let i = 0; i <= 15; i++) {
   let product = document.createElement("div"),
     productNum = document.createElement("p"),
     productNumT = document.createTextNode(i),
-    productName = document.createElement("p"),
+    productName = document.createElement("span"),
     productNameT = document.createTextNode("Product");
-    productNum.appendChild(productNumT);
-    productName.appendChild(productNameT);
-    product.appendChild(productNum);
-    product.appendChild(productName);
+  productNum.appendChild(productNumT);
+  productName.appendChild(productNameT);
+  product.appendChild(productNum);
+  product.appendChild(productName);
   content.appendChild(product);
+  main.style.setProperty("padding", "10px");
+  main.style.setProperty("background-color", "lightgrey");
+  productNum.style.setProperty("font-weight", "bold");
+  productNum.style.setProperty("font-size", "20px");
+  productName.style.setProperty("font-size", "15px");
+  productName.style.setProperty("color", "grey");
+  product.style.setProperty("background-color", "white");
+  product.style.setProperty("display", "flex");
+  product.style.setProperty("flex-direction", "column");
+  product.style.setProperty("justify-content", "center");
+  product.style.setProperty("align-items", "center");
+  product.style.setProperty("align-content", "center");
+  product.style.setProperty("padding", "1rem");
 }
 
-document.body.appendChild(content);
+main.appendChild(content);
+document.body.appendChild(main);
+// Style body
+document.body.style.setProperty("padding","0");
+document.body.style.setProperty("margin","0");
+// Style the content
+content.style.setProperty("background-color", "lightgrey");
+content.style.setProperty("display", "grid");
+content.style.setProperty("gap", "1%");
+content.style.setProperty("grid-template-columns", "repeat(3,auto)");
+content.style.setProperty("grid-template-rows", "repeat(4,auto)");
