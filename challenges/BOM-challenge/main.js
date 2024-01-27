c0f1dcbe604bf768ac28a91b3d1e768da308704d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-26 23:16:53 -0800

    created new function

--- challenges/BOM-challenge/main.js
+++ challenges/BOM-challenge/main.js
@@ -3,3 +3,19 @@ let text = document.querySelector(".input"),
   addBtn = document.querySelector(".add"),
   tasks = document.querySelector(".tasks");
 
+// Create function to add the text to tasks
+
+let addText = () => {
+  addBtn.addEventListener("click", function () {
+    if (text === "") {
+      ("");
+    } else {
+      // Create a pararaph containts the text from upper div
+      let p = document.createElement("p"),
+        pText = document.createTextNode("");
+      pText = text.innerHTML;
+      p.appendChild(pText);
+      tasks.appendChild(p);
+    }
+  });
+};
