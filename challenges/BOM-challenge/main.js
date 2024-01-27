2f04b92d2e005dc92b54b637a7b5509d7ba6a78c
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-27 07:27:52 -0800

    added some styles

--- challenges/BOM-challenge/main.js
+++ challenges/BOM-challenge/main.js
@@ -1,21 +1,37 @@
 // Declare variables
 let text = document.querySelector(".input"),
-  addBtn = document.querySelector(".add"),
-  tasks = document.querySelector(".tasks");
+  submitBtn = document.querySelector(".add"),
+  tasksDiv = document.querySelector(".tasks");
 
 // Create function to add the text to tasks
 
-let addText = () => {
-    // Create a pararaph containts the text from upper div
-      let p = document.createElement("p"),
-        pText = document.createTextNode();
-      pText = text.innerHTML;
-      p.appendChild(pText);
-      tasks.appendChild(p);
-    }
+// let addText = () => {
+// Create a pararaph containts the text from upper div
+//   let p = document.createElement("p"),
+//     pText = document.createTextNode();
 
-   
-  
+//   p.appendChild(pText);
+//   tasks.appendChild(p);
+// };
+// Generate a random ID
 
-
-window.localStorage.setItem("text", addText);
+let ranomId = function () {
+  return Math.floor(Math.random() * 1000000);
+};
+submitBtn.addEventListener("click", function () {
+  let p = document.createElement("p");
+  let pText = text.value;
+  let myText = {
+  id: ranomId(),
+    title: pText,
+  };
+  localStorage.setItem("text", myText);
+  let textB = document.createTextNode(myText.title);
+  p.appendChild(textB);
+  tasksDiv.appendChild(p);
+  p.style.setProperty("background-color", "white");
+  p.style.setProperty("padding", "15px");
+  p.style.setProperty("margin", "20px auto");
+  p.style.setProperty("width", "80%");
+  p.style.setProperty("border-radius", "6px");
+});
