213266677955ce5aa9ab658eed6bba7e090863cc
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-28 17:55:14 +0200

    some modifications to the functions

--- challenges/BOM-challenge/main.js
+++ challenges/BOM-challenge/main.js
@@ -1,42 +1,30 @@
 // Declare variables
 let text = document.querySelector(".input"),
   submitBtn = document.querySelector(".add"),
-  tasksDiv = document.querySelector(".tasks");
-
+  tasksDiv = document.querySelector(".tasks"),
+  myTasks = [];
+let p = document.createElement("p");
 // Create function to add the text to tasks
-
-// let addText = () => {
-// Create a pararaph containts the text from upper div
-//   let p = document.createElement("p"),
-//     pText = document.createTextNode();
-
-//   p.appendChild(pText);
-//   tasks.appendChild(p);
-// };
-// Generate a random ID
-
-let ranomId = function () {
-  return Math.floor(Math.random() * 1000000);
-};
-submitBtn.addEventListener("click", function () {
+let addElement = function () {
   // If the input field is empty do nothing
+
   if (text.value === "") "";
   else {
-    let p = document.createElement("p");
     let pText = text.value;
     let myText = {
-      id: ranomId(),
       title: pText,
     };
-    localStorage.setItem("text", myText);
-    let textB = document.createTextNode(myText.title);
-    p.appendChild(textB);
-    tasksDiv.appendChild(p);
-    p.style.setProperty("background-color", "white");
-    p.style.setProperty("font-size", "20px");
-    p.style.setProperty("padding", "15px");
-    p.style.setProperty("margin", "20px auto");
-    p.style.setProperty("width", "80%");
-    p.style.setProperty("border-radius", "6px");
+    localStorage.setItem("text", JSON.stringify(myText.title));
   }
-});
+};
+
+submitBtn.addEventListener("click", addElement);
+p.textContent = localStorage.getItem("text");
+tasksDiv.appendChild(p);
+p.textContent = myText.title;
+p.style.setProperty("background-color", "white");
+p.style.setProperty("font-size", "20px");
+p.style.setProperty("padding", "15px");
+p.style.setProperty("margin", "20px auto");
+p.style.setProperty("width", "80%");
+p.style.setProperty("border-radius", "6px");
