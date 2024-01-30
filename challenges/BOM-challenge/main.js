96f11ee1d182441d604af9db73d253bc5d432540
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-30 09:24:06 +0200

    fixing some issues with the code

--- challenges/BOM-challenge/main.js
+++ challenges/BOM-challenge/main.js
@@ -1,36 +1,41 @@
 // Declare variables
-let text = document.querySelector(".input"),
-  submitBtn = document.querySelector(".add"),
-  tasksDiv = document.querySelector(".tasks"),
-  myTasks = [];
-let p = document.createElement("p");
+let text = document.querySelector(".input");
+let submitBtn = document.querySelector(".add");
+let tasksDiv = document.querySelector(".tasks");
+let myTasks = [];
 // Create function to add the text to tasks
 let addElement = function () {
   let pText = text.value;
   let myText = {
-    [title]: pText,
+    title: [pText],
   };
   // If the input field is empty do nothing
-  if (text.value === "") "";
+  if (text.value === "") return;
   else {
+    myText.title = pText;
     localStorage.setItem("text", JSON.stringify(myText.title));
   }
-  tasksDiv.appendChild(document.createElement("p"));
 };
 
 myTasks.push(JSON.parse(localStorage.getItem("text")));
+console.log(myTasks);
+let createPara = () => {
+  let p = document.createElement("p");
+  for (let i = 0; i < myTasks.length; i++) {
+    p.textContent = myTasks[i];
+    console.log(myTasks);
+    p.style.setProperty("background-color", "white");
+    p.style.setProperty("font-size", "20px");
+    p.style.setProperty("padding", "15px");
+    p.style.setProperty("margin", "20px auto");
+    p.style.setProperty("width", "80%");
+    p.style.setProperty("border-radius", "6px");
+  }
+};
+submitBtn.addEventListener("click", function () {
+  tasksDiv.appendChild(p);
+});
 submitBtn.addEventListener("click", addElement);
+submitBtn.addEventListener("click", createPara);
 // Create function to create an element
-for (let i = 0; i < myTasks.length; i++) {
-  p.textContent = myTasks[i];
-  console.log(myTasks[i]);
-}
 // myTasks[myTasks.length] = localStorage.getItem("text");
-console.log(myTasks.length);
-tasksDiv.appendChild(p);
-p.style.setProperty("background-color", "white");
-p.style.setProperty("font-size", "20px");
-p.style.setProperty("padding", "15px");
-p.style.setProperty("margin", "20px auto");
-p.style.setProperty("width", "80%");
-p.style.setProperty("border-radius", "6px");
