2a4ac66fed129287067f1b24b29553666e77a07a
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-31 13:19:54 +0200

    Task one

---
+++ lesson-15/main.js
@@ -0,0 +1,15 @@
+// Task one
+// Select the div element using 15 different method
+
+document.getElementById("elzero");
+document.getElementsByClassName("element");
+document.getElementsByName("js");
+document.getElementsByTagName("div");
+document.querySelector("div");
+document.querySelector(".element");
+document.querySelector("#elzero");
+document.querySelector(`[name = "js"]`);
+document.querySelectorAll(".element");
+document.querySelectorAll("#elzero");
+document.querySelectorAll("div");
+document.querySelectorAll(`[name = "js"]`);
