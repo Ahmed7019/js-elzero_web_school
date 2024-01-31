da014a0460447cccee4d01b05cf7d948616d13c4
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-01-31 13:25:00 +0200

    task one done

--- lesson-15/main.js
+++ lesson-15/main.js
@@ -2,9 +2,12 @@
 // Select the div element using 15 different method
 
 document.getElementById("elzero");
+document.getElementById("elzero").parentNode;
 document.getElementsByClassName("element");
+document.getElementsByClassName("element")[0];
 document.getElementsByName("js");
 document.getElementsByTagName("div");
+document.getElementsByTagName("div")[0];
 document.querySelector("div");
 document.querySelector(".element");
 document.querySelector("#elzero");
