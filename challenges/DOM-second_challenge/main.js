80313d03eed6e8f6507cca9ff2efe2af49d1d534
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-02-03 08:41:51 +0200

    declared the main variables used

---
+++ challenges/DOM-second_challenge/main.js
@@ -0,0 +1,7 @@
+// Declare variables
+
+let numberInput = document.querySelector('input[name = "elements"]'),
+  textInput = document.querySelector('input[name = "texts"]'),
+  typeInput = document.querySelector('input[name = "type"]'),
+  submitBtn = document.querySelector('input[type = "submit"]'),
+  resultDiv = document.querySelector(".results");
