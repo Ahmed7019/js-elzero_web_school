2eb3975ac7f902a71a368137dcb159a41b5014f1
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-02-07 09:44:02 +0200

    A function to create elment

--- challenges/DOM-second_challenge/main.js
+++ challenges/DOM-second_challenge/main.js
@@ -1,7 +1,16 @@
-// Declare variables
+// Declare variables for input fields
 
 let numberInput = document.querySelector("input[name = 'elements']"),
   textInput = document.querySelector("input[name = 'texts']"),
   typeInput = document.querySelector("input[name = 'type']"),
   submitBtn = document.querySelector("input[type = 'submit']"),
   resultDiv = document.querySelector(".results");
+
+// Create a function to make the elements based on input type
+
+let elementType = () => {
+  let typeOfEle = elementType.value;
+  if (typeOfEle.toLowerCase() === "div") {
+    let ele = document.createElement("div");
+  } else ele = document.createElement("section");
+};
