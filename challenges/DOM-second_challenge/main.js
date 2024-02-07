ea8aa646a562720ebb34e7888cf650499b099583
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-02-07 09:53:22 +0200

    Created a function to create element

--- challenges/DOM-second_challenge/main.js
+++ challenges/DOM-second_challenge/main.js
@@ -6,11 +6,23 @@ let numberInput = document.querySelector("input[name = 'elements']"),
   submitBtn = document.querySelector("input[type = 'submit']"),
   resultDiv = document.querySelector(".results");
 
-// Create a function to make the elements based on input type
+// Create a function to take the element type
 
 let elementType = () => {
   let typeOfEle = elementType.value;
   if (typeOfEle.toLowerCase() === "div") {
     let ele = document.createElement("div");
   } else ele = document.createElement("section");
+};
+
+// Create a funciton to create an element
+
+let createEle = (ele) => {
+  // declare variables to take the values from the input fields
+  let numOfElements = numberInput.value,
+    eleText = textInput.value;
+  for (let i = 0; i < numOfElements; i++) {
+    elementType(ele);
+    ele.textContent = eleText;
+  }
 };
