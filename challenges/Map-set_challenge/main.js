5bccb307848b2bcc66c9cd19e08aa4ee2e8e00a5
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-02-12 09:54:15 +0200

    Created a set and spread to array

--- challenges/Map-set_challenge/main.js
+++ challenges/Map-set_challenge/main.js
@@ -4,8 +4,11 @@
  * You can use * Operator only in calculation
  * Set
  * Spread Operator
- * Math Object Methods
+ * Math Object Methods 
  */
 
-let n1 = [10,30,10,20];
-let n2 = [30,20,10]
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+
+console.log(new Set());
+console.log([...new Set([...n2, ...n1])]); // 210
