e65da38f2c6b906efb3e5313e1240aab3c96959b
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-05 18:07:25 +0200

    appended the elements correctly

--- challenges/DOM-second_challenge/main.js
+++ challenges/DOM-second_challenge/main.js
@@ -3,34 +3,46 @@
 let numberInput = document.querySelector(`[name = "elements"]`);
 let textInput = document.querySelector(`[name = "texts"]`),
   typeInput = document.querySelector(`[name = "type"]`),
+  div = document.querySelector(`[value = "Div"]`),
+  section = document.querySelector(`[value = "Section"]`),
   submitBtn = document.querySelector(`[name = "create"]`),
   resultDiv = document.querySelector(".results");
 // Create a function to take the element type
 let elementType = () => {
-  let typeOfEle = elementType.value;
-  if (typeOfEle.toLowerCase() === "div") {
+  let typeOfEle = typeInput.value,
+    x = textInput.value;
+  if (typeInput === div) {
     let ele = document.createElement("div");
+    ele = document.createElement("section");
+    ele.style.setProperty("padding", "1rem");
+    ele.style.setProperty("width", "200px");
+    ele.style.setProperty("border-radius", "8px");
+    ele.style.setProperty("background-color", "blue");
+    ele.style.setProperty("color", "white");
     resultDiv.appendChild(ele);
   } else {
     ele = document.createElement("section");
+    ele.style.setProperty("padding", "1rem");
+    ele.style.setProperty("width", "200px");
+    ele.style.setProperty("border-radius", "8px");
+    ele.style.setProperty("background-color", "blue");
+    ele.style.setProperty("color", "white");
     resultDiv.appendChild(ele);
   }
 };
 // Create a funciton to create an element
 
-let createEle = (ele) => {
+let createEle = () => {
   // declare variables to take the values from the input fields
   let numOfElements = numberInput.value,
     eleText = textInput.value;
   for (let i = 0; i < numOfElements; i++) {
-    elementType(ele);
-    ele.textContent = eleText;
-    resultDiv.appendChild(ele);
+    elementType();
   }
 };
 // Add eventlistener to the button
 
-submitBtn.addEventListener("click", createEle());
+submitBtn.addEventListener("click", createEle);
 
 // prevent form submittion
 
