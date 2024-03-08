ba61bc3515b039d4ffce578ab63647148ad12f1d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-08 11:44:15 +0200

    challenge done

--- challenges/DOM-second_challenge/main.js
+++ challenges/DOM-second_challenge/main.js
@@ -6,7 +6,8 @@ let textInput = document.querySelector(`[name = "texts"]`),
   div = document.querySelector(`[value = "Div"]`),
   section = document.querySelector(`[value = "Section"]`),
   submitBtn = document.querySelector(`[name = "create"]`),
-  resultDiv = document.querySelector(".results");
+  resultDiv = document.querySelector(".results"),
+  arr = [];
 // Create a function to take the element type
 let elementType = () => {
   let typeOfEle = typeInput.value,
@@ -21,6 +22,9 @@ let elementType = () => {
     ele.style.setProperty("text-align", "center");
     ele.textContent = x;
     resultDiv.appendChild(ele);
+    for (let i = 0; i < numberInput.value; i++) {
+      localStorage.setItem(`elements${[i]}`, arr.push(ele));
+    }
   } else {
     ele = document.createElement("section");
     ele.style.setProperty("padding", "1rem");
@@ -33,6 +37,12 @@ let elementType = () => {
     resultDiv.appendChild(ele);
   }
 };
+
+// Load elements of page refresh
+window.addEventListener("load", () => {
+  for (let i = 0; i < numberInput.value; i++) resultDiv.appendChild(ele[i]);
+});
+
 // Create a funciton to create an element
 
 let createEle = () => {
