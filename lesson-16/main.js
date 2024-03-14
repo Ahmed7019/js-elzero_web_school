71032b9217507de84ed8dc5e2c2e3932c6751ce8
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-14 09:30:25 +0200

    added functions correctly

--- lesson-16/main.js
+++ lesson-16/main.js
@@ -1,28 +1,29 @@
 // Task two
 
-let addClass = document.querySelector(".classes-to-add"),
-  rmClass = document.querySelector(".classes-to-remove"),
-  current = document.querySelector(`[title = "Current"]`),
-  classes = document.querySelector(".classes-list div");
+let inputToAddClasses = document.querySelector(".classes-to-add");
+let inputToRmClasses = document.querySelector(".classes-to-remove");
+let current = document.querySelector(".element.current");
+let classes = document.querySelector(".assign .classes-list div");
 
-let add = (x) => {
-  //   x.style.setProperty("background-color", "red");
-  //   x.style.setProperty("color", "white");
-  //   x.style.setProperty("padding", "4px 8px");
-  //   x.style.setProperty("margin", "0 10px");
-  //   x.style.setProperty("border-radius", "8px");
-  //   classes.style.setProperty("display", "flex");
-  //   classes.style.setProperty("justify-content", "center");
-  //   classes.style.setProperty("align-items", "center");
+let addClass = (x) => {
   current.classList.add(x);
-  x = document.createElement("span");
-  classes.appendChild(x);
 };
 
 let rm = (x) => {
   classes.remove(x);
 };
 
-addClass.addEventListener("input", () => {
-  add(addClass.value);
+// Function to create spans
+let addSpan = () => {
+  let sp = document.createElement("span");
+  for (let i = 0; i < current.classList.length; i++) {
+    sp.textContent = current.classList.toString().toLowerCase().split(" ")[i];
+    classes.appendChild(sp);
+  }
+};
+
+inputToAddClasses.addEventListener("blur", () => {
+  addClass(inputToAddClasses.value);
+  addSpan();
+  inputToAddClasses.value = "";
 });
