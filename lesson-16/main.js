5074b7318bf72f0a41ca84a8b598bac4c3527ce3
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-16 15:56:55 +0200

    Created soan element on bluring

--- lesson-16/main.js
+++ lesson-16/main.js
@@ -13,17 +13,36 @@ let rm = (x) => {
   classes.remove(x);
 };
 
-// Function to create spans
+// Function to create span element with content inside
 let addSpan = () => {
-  let sp = document.createElement("span");
   for (let i = 0; i < current.classList.length; i++) {
+    let sp = document.createElement("span");
+    // The content within the span is the classes of the current element
     sp.textContent = current.classList.toString().toLowerCase().split(" ")[i];
     classes.appendChild(sp);
   }
 };
 
 inputToAddClasses.addEventListener("blur", () => {
-  addClass(inputToAddClasses.value);
-  addSpan();
-  inputToAddClasses.value = "";
+  if (current.classList.length > 0) {
+    classes.textContent = "";
+    // check if there is a class at the begining
+    addSpan();
+    current.classList = "";
+  } else if (inputToAddClasses.value != "") {
+    // added class from input field
+    classes.textContent = "";
+    addClass(inputToAddClasses.value);
+    addSpan();
+    inputToAddClasses.value = "";
+    current.classList = "";
+  } else classes.textContent = "No class found";
+});
+
+inputToRmClasses.addEventListener("blur", (classToRemove) => {
+  if (current.classList.contains(classToRemove)) {
+    rm(classToRemove);
+  } else {
+    classes.textContent("Class not found");
+  }
 });
