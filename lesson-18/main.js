b774925c77f13e08a47319c182dda85eb4e2286a
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-21 12:46:52 +0200

    task one done

--- lesson-18/main.js
+++ lesson-18/main.js
@@ -14,14 +14,26 @@ let sizes = document.querySelector("#sizes");
 // On selection , change the font family
 fonts.addEventListener("change", () => {
   document.body.style.fontFamily = fonts.value;
+  localStorage.setItem("font-family", fonts.value);
 });
 
 // On selection , change the color
 colors.addEventListener("change", () => {
   document.body.style.color = colors.value;
+  localStorage.setItem("color", colors.value);
 });
 
-// On selection , change the color
+// On selection , change font size
 sizes.addEventListener("change", () => {
   document.body.style.fontSize = `${sizes.value}px`;
+  localStorage.setItem("font-size", `${sizes.value}`);
+});
+
+window.addEventListener("load", () => {
+  document.body.style.fontFamily = localStorage.getItem("font-family");
+  document.body.style.color = localStorage.getItem("color");
+  document.body.style.fontSize = `${localStorage.getItem("font-size")}px`;
+  fonts.value = localStorage.getItem("font-family");
+  colors.value = localStorage.getItem("color");
+  sizes.value = localStorage.getItem("font-size");
 });
