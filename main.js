2162c7749efe23e8276f5e5d947dbdfbec61530d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-22 15:29:34 +0200

    task three

--- main.js
+++ main.js
@@ -19,8 +19,24 @@ let mySkills = [
 
 // Write Your Destructuring Assignment Here
 
-let [a, b, c, [d, e, [f, g]]] = mySkills;
+// let [a, b, c, [d, e, [f, g]]] = mySkills;
 
-console.log(`My Skills: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}, ${g}`);
+// console.log(`My Skills: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}, ${g}`);
 
 // My Skills: HTML, CSS, JavaScript, PHP, Python, Django, Laravel
+
+// Task three
+
+let arr1 = ["Ahmed", "Sameh", "Sayed"];
+let arr2 = ["Mohamed", "Gamal", "Amir"];
+let arr3 = ["Haytham", "Shady", "Mahmoud"];
+
+// Play With Arrays To Prepare For Destructuring
+// Write Your Destructuring Assignment Here
+
+let myFriends = [...arr3, ...arr1];
+
+let [, a, b, c, , ,] = myFriends;
+console.log(`My Best Friends: ${a}, ${b}, ${c}`);
+
+// My Best Friends: Shady, Mahmoud, Ahmed
