f7ab20264cc30237e55bfb93b40f0bf9f927d83b
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-03-30 17:43:38 +0200

    Task two

--- lesson-20/main.js
+++ lesson-20/main.js
@@ -8,3 +8,12 @@ for (let i = 0; i < setOfNumbers.size; i++) {
   if (lastEle.next().value === 2) console.log(2);
 }
 console.log(setOfNumbers.size);
+
+// Task two
+
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+myFriends.sort();
+let myUniqueFriends = new Set(myFriends);
+console.log(myUniqueFriends);
+// Needed Output
+// (4)[("Ahmed", "Mahmoud", "Osama", "Sayed")];
