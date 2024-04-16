f6c0efe0f91c01fa1e94ac6e2af5b455680d6dec
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-04-16 10:58:14 +0200

    task two done

--- lesson-21/main.js
+++ lesson-21/main.js
@@ -5,3 +5,13 @@ let taskOneIp = "2001:db8:3333:4444:5555:6666:7777:8888";
 let regExpOne = /\d+.\w+.(\d+|.)+/gi;
 
 console.log(taskOneIp.match(regExpOne));
+
+// Task two
+
+let specialNames = "Os10O OsO Os100O Osa100O Os1000 Os100m";
+
+let specialNamesRegExp = /Os\d*O/gi;
+
+console.log(specialNames.match(specialNamesRegExp));
+// Output
+// ['Os10O', 'OsO', 'Os100O']
