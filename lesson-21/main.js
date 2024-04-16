bd0fd423301e6f0a3f8cde8911cc5896513fa677
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-04-16 12:33:02 +0200

    Task five done

--- lesson-21/main.js
+++ lesson-21/main.js
@@ -43,3 +43,17 @@ let re = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/i;
  * .* => non line terminators (May exist for once or not at all)
  *  i => (Modifier) Case insensitive matching
  */
+
+// Task five
+
+let date1 = "25/10/1982";
+let date2 = "25 - 10 - 1982";
+let date3 = "25 10 1982";
+let date4 = "25 10 82";
+
+let dateRe = /\d+\W+\d+\W+\d+/ig; // Write Pattern Here
+
+console.log(date1.match(dateRe)); // "25/10/1982"
+console.log(date2.match(dateRe)); // "25 - 10 - 1982"
+console.log(date3.match(dateRe)); // "25 10 1982"
+console.log(date4.match(dateRe)); // "25 10 82"
