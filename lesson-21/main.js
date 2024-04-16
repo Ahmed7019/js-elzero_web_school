71d7b08068519b49c36d9966a59351d3112564f0
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-04-16 12:26:49 +0200

    task four done

--- lesson-21/main.js
+++ lesson-21/main.js
@@ -23,3 +23,23 @@ let phone = "+(995)-123 (4567)";
 let phoneRegExp = /(\D+|\d+|\s)+/gi;
 
 console.log(phone.match(phoneRegExp));
+
+// Task four
+
+let re = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/i;
+
+/**
+ * https? => It may start with https or not
+ * : => match ":"
+ * \/\/ => match the double back slash
+ * ? => Something might be here or not
+ * : => match this sign
+ * [-\w]+ => Add the sign - then follow it up with character / number anything (It could be one or more character)
+ * \. => match any character except new line or other line terminator
+ * \w+ => match any word character (one or more)
+ * (?:\.\w+)? => match a (? zero or none ) "the sign :" non line terminators + word characters ) all these may exist or not
+ * \ => escape character
+ * /? => match a question mark
+ * .* => non line terminators (May exist for once or not at all)
+ *  i => (Modifier) Case insensitive matching
+ */
