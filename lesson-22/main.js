3fa5aadea01f61a8eddf8d84515a9f613f504987
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-13 10:00:15 +0200

    task two done

--- lesson-22/main.js
+++ lesson-22/main.js
@@ -5,7 +5,7 @@ function Car(name, model, price) {
   this.p = price;
 }
 
-class Viechel {
+class Vehicel {
   constructor(name, model, price) {
     this.n = name;
     this.m = model;
@@ -25,7 +25,7 @@ class Viechel {
   };
 }
 
-let carOne = new Viechel("corolla", 2011, 4500);
+let carOne = new Vehicel("corolla", 2011, 4500);
 
 carOne.info();
 carOne.run();
@@ -35,3 +35,42 @@ console.log(carOne);
 
 // ("Car One Name Is MG And Model Is 2022 And Price Is 420000");
 // ("Car Is Running Now");
+
+// Task two
+
+console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+
+class Phone {
+  constructor(name, serial, price) {
+    this.name = name;
+    this.serial = serial;
+    this.price = price;
+  }
+}
+
+// Write Tablet Class Here
+class Tablet extends Phone {
+  constructor(name, serial, price, size = "Unknown") {
+    super(name, serial, price);
+    this.s = size;
+  }
+
+  fullDetails = () => {
+    console.log(
+      `${this.name} serial is ${this.serial} and size is ${this.s}`
+    );
+  };
+}
+
+let TabletOne = new Tablet("iPad", 100200300, 1500, 12.9);
+let TabletTwo = new Tablet("Nokia", 350450650, 800, 10.5);
+let TabletThree = new Tablet("LG", 250450650, 650);
+
+TabletOne.fullDetails();
+// iPad Serial is 100200300 And Size Is 12.9
+
+TabletTwo.fullDetails();
+// Nokia Serial is 350450650 And Size Is 10.5
+
+TabletThree.fullDetails();
+// LG Serial is 250450650 And Size Is Unknown
