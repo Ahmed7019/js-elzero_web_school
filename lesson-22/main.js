6e3ce60dcd0db0bdc7da6514983ee713f3f14498
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-15 10:26:33 +0200

    added task five

--- lesson-22/main.js
+++ lesson-22/main.js
@@ -140,3 +140,29 @@ String.prototype.addLove = function addLove() {
 // Do Not Edit Below
 let myStr = "Elzero";
 console.log(myStr.addLove()); // I Love Elzero Web School
+
+// Task five
+
+console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+const myObj = {
+  username: "Elzero",
+  id: 100,
+  score: 1000,
+  country: "Egypt",
+};
+
+// Write Your Code Here
+
+myObj.score = 500;
+
+for (let prop in myObj) {
+  console.log(`${prop} => ${myObj[prop]}`);
+}
+
+console.log(myObj);
+
+// Needed Output
+
+"username => Elzero"
+"score => 1000"
+{username: 'Elzero', score: 1000, id: 100}
