7ad1430f0e67b36ad4c4c728209b29710c213f9c
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-18 00:47:42 +0200

    Task two done

--- lesson-23/main.js
+++ lesson-23/main.js
@@ -2,10 +2,22 @@
 
 let myBirthday = new Date("January 25, 2003"),
   today = new Date(),
-  age =  today - myBirthday;
+  age = today - myBirthday;
 console.log(`${parseInt(age * 86400)} Seconds`);
 console.log(`${parseInt(age / 1000 / 60)} Minutes`);
 console.log(`${parseInt(age / 1000 / 60 / 60)} Hours`);
 console.log(`${parseInt(age / 1000 / 60 / 60 / 24)} Days`);
 console.log(`${parseInt(age / 1000 / 60 / 60 / 24 / 30)}  Months`);
 console.log(`${parseInt(age / 1000 / 60 / 60 / 24 / 365)} Years`);
+
+// task two
+
+// Needed Output
+
+let oldDate = new Date(0);
+oldDate.setFullYear(1980);
+oldDate.setSeconds(1);
+oldDate.setHours(0);
+console.log(oldDate);
+
+// "Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
