6aefab6f3c8d102354f8f6852a14d01fa4fa733d
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-18 01:02:40 +0200

    task three

--- lesson-23/main.js
+++ lesson-23/main.js
@@ -11,7 +11,7 @@ console.log(`${parseInt(age / 1000 / 60 / 60 / 24 / 30)}  Months`);
 console.log(`${parseInt(age / 1000 / 60 / 60 / 24 / 365)} Years`);
 
 // task two
-
+console.log(`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-`);
 // Needed Output
 
 let oldDate = new Date(0);
@@ -21,3 +21,15 @@ oldDate.setHours(0);
 console.log(oldDate);
 
 // "Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
+console.log(`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-`);
+console.log(`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-`);
+
+// task three
+
+let taskThree = new Date();
+taskThree.setFullYear(2024, taskThree.getMonth(), 0);
+console.log(taskThree);
+// Needed Output
+
+// ("Sat Apr 30 2022 18:13:20 GMT+0200 (Eastern European Standard Time)");
+// ("Previous Month Is April And Last Day Is 30");
