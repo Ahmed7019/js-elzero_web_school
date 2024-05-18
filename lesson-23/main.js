38096f7f540cdda73aeaf79c2fcec99010797183
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-18 10:54:21 +0200

    task six done

--- lesson-23/main.js
+++ lesson-23/main.js
@@ -72,3 +72,35 @@ console.log(generator.next()); // {value: 2714, done: false}
 console.log(generator.next()); // {value: 3854, done: false}
 console.log(generator.next()); // {value: 5194, done: false}
 console.log(generator.next()); // {value: 6734, done: false}
+
+console.log(`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-`);
+console.log(`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-`);
+
+// Task six
+
+function* genNumbers() {
+  yield* [1, 2, 2, 2, 3, 4, 5];
+}
+function* genLetters() {
+  yield* ["A", "B", "B", "B", "C", "D"];
+}
+
+// Write Your Generator Function Here
+function* genAll() {
+  let x = new Set(genNumbers());
+  yield* x;
+  let y = new Set(genLetters());
+  yield* y;
+}
+
+let generatorTaskSix = genAll();
+
+console.log(generatorTaskSix.next()); // {value: 1, done: false}
+console.log(generatorTaskSix.next()); // {value: 2, done: false}
+console.log(generatorTaskSix.next()); // {value: 3, done: false}
+console.log(generatorTaskSix.next()); // {value: 4, done: false}
+console.log(generatorTaskSix.next()); // {value: 5, done: false}
+console.log(generatorTaskSix.next()); // {value: "A", done: false}
+console.log(generatorTaskSix.next()); // {value: "B", done: false}
+console.log(generatorTaskSix.next()); // {value: "C", done: false}
+console.log(generatorTaskSix.next()); // {value: "D", done: false}
