527ec8c7ebca3a2576157bcdaadb99737a761c14
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-22 07:28:28 +0200

    task two done :)

--- lesson-24/main.js
+++ lesson-24/main.js
@@ -2,10 +2,15 @@
 let request = new XMLHttpRequest();
 request.open("GET", "./articles.json");
 request.send();
-console.log(request);
 request.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     console.log(this.responseText);
+    let data = request.responseText;
+    const mainData = JSON.parse(data);
+    for (let i = 0; i < mainData.length; i++) mainData[i]["category"] = "All";
+    console.log(mainData);
+    const updatedData = JSON.stringify(mainData);
+    console.log(updatedData);
   }
   this.onloadend = function () {
     console.log("Data loaded");
