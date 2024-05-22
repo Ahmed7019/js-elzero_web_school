edc466857af9fcd6fd97978d155d1b3c63622677
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-05-22 09:08:15 +0200

    last task done :)

--- lesson-24/main.js
+++ lesson-24/main.js
@@ -7,10 +7,24 @@ request.onreadystatechange = function () {
     console.log(this.responseText);
     let data = request.responseText;
     const mainData = JSON.parse(data);
-    for (let i = 0; i < mainData.length; i++) mainData[i]["category"] = "All";
+    // for (let i = 0; i < mainData.length; i++) mainData[i]["category"] = "All";
     console.log(mainData);
     const updatedData = JSON.stringify(mainData);
     console.log(updatedData);
+    for (let i = 0; i < mainData.length; i++) {
+      let mainDiv = document.createElement("div");
+      mainDiv.setAttribute("id", "data");
+      document.body.appendChild(mainDiv);
+      mainDiv.innerHTML = ` 
+      <div>
+        <h2>${mainData[i]["title"]}</h2>
+        <p>${mainData[i]["body"]}</p>
+        <p>Author : ${mainData[i]["author"]}</p>
+        <p>Category : ${mainData[i]["category"]}</p>
+      </div>
+        
+        `;
+    }
   }
   this.onloadend = function () {
     console.log("Data loaded");
